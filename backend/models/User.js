35fc2bcd3ad4152d115ba3961495527527d89dd1
ProPotam <yotamofri.b@gmail.com>
2023-11-01 10:16:16 +0200

    UserSchema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,2 +1,21 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  workouts: [
+    {
+      type: Object,
+    },
+  ],
+});
+module.exports = userSchema;
