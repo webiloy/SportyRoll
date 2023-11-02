6758c504ac5fa863c508bba72e21ead4284d60e4
Daniel Moses <dm98912@gmail.com>
2023-11-03 00:32:50 +0200

    Added How it Works section - not finished

--- frondend/src/pages/home/Home.jsx
+++ frondend/src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import { HeroSec } from "./components/HeroSec";
 import { Features } from "./components/Features";
+import { Hiw } from "./components/Hiw";
 export function Home() {
   return (
     <>
-      <HeroSec></HeroSec> <Features></Features>
+      <HeroSec></HeroSec> <Features></Features> <Hiw></Hiw>
     </>
   );
 }
--- frondend/src/pages/home/components/Features.jsx
+++ frondend/src/pages/home/components/Features.jsx
@@ -9,17 +9,17 @@ export function Features() {
             <h1 className="text-white font-bold text-2xl text-center lg:text-left">
               Extensive Collection of Certified Trainer-Curated Workouts
             </h1>
-            <ul className="text-secondary-text text-base  flex flex-col gap-6 child:flex child:items-start child:gap-1 my-8">
+            <ul className="text-secondary-text text-sm  flex flex-col gap-6 child:flex child:items-start child:gap-1 my-8">
               <li>
-                <AiOutlineCheck className="text-2xl flex-shrink-0"></AiOutlineCheck>{" "}
+                <AiOutlineCheck className="text-xl flex-shrink-0"></AiOutlineCheck>
                 Get fitness plans crafted by experts
               </li>
               <li>
-                <AiOutlineCheck className="text-2xl flex-shrink-0"></AiOutlineCheck>{" "}
+                <AiOutlineCheck className="text-xl flex-shrink-0"></AiOutlineCheck>
                 Choose from more than 20 personalized routines
               </li>
               <li>
-                <AiOutlineCheck className="text-2xl flex-shrink-0"></AiOutlineCheck>{" "}
+                <AiOutlineCheck className="text-xl flex-shrink-0"></AiOutlineCheck>
                 Track your fitness journey effortlessly
               </li>
               <li className="text-secondary underline">
@@ -29,34 +29,32 @@ export function Features() {
           </div>
         </div>
       </div>
+
       {/* Second Feature */}
 
-      <div
-        // style={{ backgroundImage: `url(${featuresOne})` }}
-        className="flex flex-col bg-gradient-to-br from-black to-[#141414] min-h-[450px] md:bg-features-one md:h-[90dvh] w-screen z-40 bg-no-repeat bg-cover overflow-hidden justify-center "
-      >
+      <div className="flex flex-col bg-gradient-to-br from-black to-[#141414] min-h-[450px] md:bg-features-one md:h-[90dvh] w-screen z-40 bg-no-repeat bg-cover overflow-hidden justify-center ">
         <div className="mb-4 bg-football-team w-screen h-72 bg-cover bg-center md:hidden"></div>
         <div className="flex flex-col gap-16  px-10 lg:px-24 md:px-16">
           <div className="relative bottom-20">
             <h1 className="text-white font-bold text-2xl text-center lg:text-left">
               Your Supportive Fitness Community
             </h1>
-            <ul className="text-secondary-text text-base  flex flex-col gap-6 child:flex child:items-start child:gap-1 mt-8">
+            <ul className="text-secondary-text text-sm  flex flex-col gap-6 child:flex child:items-start child:gap-1 mt-8">
               <li>
-                <AiOutlineCheck className="text-2xl flex-shrink-0"></AiOutlineCheck>
+                <AiOutlineCheck className="text-xl flex-shrink-0"></AiOutlineCheck>
                 Join like-minded individuals for support
               </li>
               <li>
-                <AiOutlineCheck className="text-2xl flex-shrink-0"></AiOutlineCheck>
+                <AiOutlineCheck className="text-xl flex-shrink-0"></AiOutlineCheck>
                 Find motivation in the inspiring success stories of our
                 community members
               </li>
               <li>
-                <AiOutlineCheck className="text-2xl flex-shrink-0"></AiOutlineCheck>
+                <AiOutlineCheck className="text-xl flex-shrink-0"></AiOutlineCheck>
                 Access guidance from trainers and members
               </li>
               <li>
-                <AiOutlineCheck className="text-2xl flex-shrink-0"></AiOutlineCheck>
+                <AiOutlineCheck className="text-xl flex-shrink-0"></AiOutlineCheck>
                 Share your progress and achieve your fitness goals
               </li>
             </ul>
