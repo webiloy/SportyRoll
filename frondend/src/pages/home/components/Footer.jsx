2d2667353a620ef93b0ec82dd9d48ce020497ae6
Daniel Moses <dm98912@gmail.com>
2023-11-03 14:04:29 +0200

    Added contact section

---
+++ frondend/src/pages/home/components/Footer.jsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <>
+      {/* Contact us section */}
+      <div className="flex justify-between p-6 bg-gradient-to-br from-secondary to-[#13EA4F] items-center">
+        <h2 className="text-text text-lg font-semibold ">Have a Question?</h2>
+        <button className="rounded-full p-3 px-5 bg-text">
+          <span className="bg-gradient-to-r from-secondary to-[#13EA4F] bg-clip-text text-transparent">
+            Contact us
+          </span>
+        </button>
+      </div>
+      {/* Footer */}
+    </>
+  );
+}
